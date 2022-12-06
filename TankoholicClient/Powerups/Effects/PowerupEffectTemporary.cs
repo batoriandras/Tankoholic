ddda2bd8e98fa8901bdf00c0ba96a0a30c7eb61c
Isakov <isakov.erik20021@gmail.com>
2022-12-06 10:54:10 +0100

    refactore powerup classes

---
+++ TankoholicClient/Powerups/Effects/PowerupEffectTemporary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankoholicClient
+{
+    public abstract class PowerupEffectTemporary : PowerupEffect
+    {
+        public Action OnEnd;
+
+        public int Duration { get => 10; }
+
+        public void Start()
+        {
+            Task.Delay(Duration * 1000).ContinueWith(task => {
+                OnEnd();
+            });
+        }
+    }
+}
---
+++ TankoholicClient/Powerups/PowerupEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TankoholicClient.Powerup
+{
+    public class PowerupEntity : Entity
+    {
+        private static PowerupEffect[] allPowerupEffects = new PowerupEffect[] { 
+            new SpeedUpPowerup(), new HealthPowerup()
+        };
+
+        public PowerupEffect Effect { get; private set; }
+
+        public PowerupEntity(PowerupEffect effect)
+        {
+            this.Effect = effect;
+        }
+
+        public static PowerupEntity RandomPowerup()
+        {
+            int index = new Random().Next(allPowerupEffects.Length);
+            return new PowerupEntity(allPowerupEffects[index]);
+        }
+
+        public override void Draw(ref SpriteBatch spriteBatch, ref Texture2D rectangleBlock)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
