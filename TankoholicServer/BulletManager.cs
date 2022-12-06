91a0ba0d9a04ef308f650e973d83945cc968fa43
szogipeti <szogipeti02@gmail.com>
2022-12-06 20:43:47 +0100

    Send recieved bullets to clients

---
+++ TankoholicServer/BulletManager.cs
@@ -0,0 +1,29 @@
+﻿using Riptide;
+using TankoholicClassLibrary;
+
+namespace TankoholicServer;
+
+public class BulletMessageManager
+{
+    [MessageHandler((ushort)MessageIds.BULLET_SPAWN)]
+    public static void SendSpawnMessage(ushort id, Message message)
+    {
+        Program.Server!.SendToAll(message);
+    }
+    [MessageHandler((ushort)MessageIds.BULLET_POSITION)]
+    private static void Position(ushort id, Message message)
+    {
+        var position = message.GetFloats();
+        Program.Server!.SendToAll(CreatePositionMessage(id, position));
+
+        if (!Program.DebugPosition) return;
+        ServerDebug.Warn($"Bullet({id}) new position: X:{position[0]} Y:{position[1]}");
+    }
+    private static Message CreatePositionMessage(ushort id, float[] position)
+    {
+        Message message = Message.Create(MessageSendMode.Reliable, MessageIds.BULLET_POSITION);
+        message.AddUShort(id);
+        message.AddFloats((float[])position);
+        return message;
+    }
+}
